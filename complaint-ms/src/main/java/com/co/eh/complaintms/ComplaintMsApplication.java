e96d0f047c2b366daf0a49c6e990613f0413f463
mateohenao <matteohenao36@gmail.com>
2022-09-07 16:08:55 -0500

    init commit

---
+++ complaint-ms/src/main/java/com/co/eh/complaintms/ComplaintMsApplication.java
@@ -0,0 +1,13 @@
+package com.co.eh.complaintms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintMsApplication.class, args);
+	}
+
+}
---
+++ complaint-ms/src/test/java/com/co/eh/complaintms/ComplaintMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.eh.complaintms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
